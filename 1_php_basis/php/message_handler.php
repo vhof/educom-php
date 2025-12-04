ac608986503fbc83ed475898270f9045527f78a6
vhof <v-vant_hof@live.nl>
2025-12-04 13:43:01 +0100

    extracted strings in message_handler to variables

--- 1_php_basis/php/message_handler.php
+++ 1_php_basis/php/message_handler.php
@@ -4,20 +4,23 @@
 ?>
 <?php 
     $valid_message = false;
-    $fields = array("name", "email", "message");
-    $values = array("name" => "", "email" => "", "message" => "");
-    $error = array("name" => "", "email" => "", "message" => "");
+    $name_str = "name";
+    $email_str = "email";
+    $message_str = "message";
+    $fields = array($name_str, $email_str, $message_str);
+    $values = array($name_str => "", $email_str => "", $message_str => "");
+    $error = array($name_str => "", $email_str => "", $message_str => "");
     
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $valid_message = true;
 
-        $values["name"] = cleanInput($_POST["name"]);
-        $values["email"] = cleanInput($_POST["email"]);
-        $values["message"] = cleanInput($_POST["message"]);
+        $values[$name_str] = cleanInput($_POST[$name_str]);
+        $values[$email_str] = cleanInput($_POST[$email_str]);
+        $values[$message_str] = cleanInput($_POST[$message_str]);
 
         // Validate email
-        if (!filter_var($values["email"], FILTER_VALIDATE_EMAIL)) {
-            $error["email"] = "Invalid email format";
+        if (!filter_var($values[$email_str], FILTER_VALIDATE_EMAIL)) {
+            $error[$email_str] = "Invalid email format";
             $valid_message = false;
         }
 
